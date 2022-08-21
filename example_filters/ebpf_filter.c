15e2aa066872aea98e8437c30195a81b719cda08
Anthea Chen <anxuec2@illinois.edu>
2022-08-20 23:01:24 -0500

    consider cross architecture c struct padding difference

--- example_filters/ebpf_filter.c
+++ example_filters/ebpf_filter.c
@@ -6,6 +6,7 @@
 #include <linux/icmp.h>
 #include <linux/in.h>
 #include <stddef.h>
+#include <stdint.h>
 #include "bpf_helpers.h"
 #include "bpf_legacy.h"
 
@@ -19,8 +20,7 @@
 
 /* Helper macro to print out debug messages */
 #define printk(fmt, ...)                               \
-	(                                                  \
-		{                                              \
+	(                                                  \ {                                              \
 			char ____fmt[] = fmt;                      \
 			bpf_trace_printk(____fmt, sizeof(____fmt), \
 							 ##__VA_ARGS__);           \
@@ -28,36 +28,49 @@
 
 char __license[] __section("license") = "GPL";
 
-__section("icmp_filter") int classifier_icmp(struct __sk_buff *skb)
+#define ETHHDR_LEN 14
+#define ETHHDR_HPROTO_OFFSET 12
+#define IPHDR_LEN_MIN 20
+#define IPHDR_LEN_MAX 24
+#define IPHDR_VERSION_OFF ETHHDR_LEN
+#define IPHDR_PROTO_OFF ETHHDR_LEN + 9
+#define ICMPHDR_OFF ETHHDR_LEN+IPHDR_LEN_MIN 
+#define ICMPHDR_TYPE_OFF ICMPHDR_OFF 
+#define ICMPHDR_ID_OFF ICMPHDR_OFF + 4 
+
+__section("icmp_filter") int32_t classifier_icmp(struct __sk_buff *skb)
 {
-	// data is at start of ethernet header
-	// (data+14) is start of IP header
-	// (data+34) is start of ICMP header
-	//
-	//
-	//
-	if (load_half(skb, offsetof(struct ethhdr, h_proto)) != ETH_P_IP)
+	// Memory buffer is at start of ethernet header
+	
+	// Incoming IP packet
+	if (load_half(skb, ETHHDR_HPROTO_OFFSET) != __constant_htons(ETH_P_IP))
 	     return 0;
 
-	if ((load_byte(skb, sizeof(struct ethhdr)) & 0xF) != 4)
+    // Check version field of ip header 
+	if ((load_byte(skb, ETHHDR_LEN) & 0xF) != 4)
 	     return 0;
 
-	if (load_byte(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, protocol)) != 1)
+    // Check IP packet protocol for ICMP
+	if (load_byte(skb, IPHDR_PROTO_OFF) != 1)
 	     return 0;
 
-	if (load_half(skb, sizeof(struct ethhdr) + sizeof(struct iphdr)) != 0)
+    // Check ICMP type for reply
+	if (load_half(skb, ICMPHDR_TYPE_OFF) != 0)
 	     return 0;
     
-	if (load_half(skb, sizeof(struct ethhdr) + sizeof(struct iphdr) + 4) != 5566)
+    // Check ICMP id
+	if (load_half(skb, ICMPHDR_ID_OFF) != 5566)
 	     return 0;
-//	void *data = (void *)(long)skb->data;
-//	void *data_end = (void *)(long)skb->data_end;
-//	struct ethhdr *eth = data;
-//	struct iphdr *iph = data + 14;
-//	struct icmphdr *icmph = data + 34;
+
+	return 65535; 
+//	if (load_byte(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, protocol)) != 1)
+//	     return 0;
 //
-//	if (data + 34 > data_end)
-//        return 0;
+//	if (load_half(skb, sizeof(struct ethhdr) + sizeof(struct iphdr)) != 0)
+//	     return 0;
+//    
+//	if (load_half(skb, sizeof(struct ethhdr) + sizeof(struct iphdr) + 4) != 5566)
+//	     return 0;
 //
 //	if (eth->h_proto != __constant_htons(ETH_P_IP))
 //        return 0;
@@ -79,5 +92,4 @@ __section("icmp_filter") int classifier_icmp(struct __sk_buff *skb)
 //	printk("id=%d seq=%d code=%d", htons(icmph->un.echo.id), htons(icmph->un.echo.sequence), icmph->code);
 	
 
-	return -1;
 }
